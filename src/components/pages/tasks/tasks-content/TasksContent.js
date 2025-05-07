3a06b9313e24ce592df4876b39cafb146929de3c
Mahmoud123-Makhaly <mahmoud.abdelfatah@5d-agency.com>
2025-05-07 09:45:06 +0300

    create 5d sashboard

---
+++ src/components/pages/tasks/tasks-content/TasksContent.js
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import { Dashboard } from 'react-employee-calendar'
+import 'react-employee-calendar/dist/index.css'
+
+const TasksContent = () => {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  )
+}
+export default TasksContent
